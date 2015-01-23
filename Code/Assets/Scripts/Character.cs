d43494eb3a1e0fd0e5c9a1e96b7f57c10f870ae6
Anton Roy <anton.francis.roy@gmail.com>
2015-01-23 23:02:28 +0100

    character code

---
+++ Code/Assets/Scripts/Character.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    private float Speed = 2.5f;
+    private float positionThreshold = .1f;
+
+    private Vector3 TargetPosition;
+    private bool IsMoving = false;
+
+    public void SetTargetPosition(Vector3 target)
+    {
+        TargetPosition = target;
+        IsMoving = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+        IsMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (IsMoving)
+        { 
+            Vector3 curPos = transform.position;
+            Vector3 displacement = (TargetPosition - curPos).normalized * Speed * Time.deltaTime;
+            Vector3 nextPos = transform.position + displacement;
+            if ((TargetPosition - nextPos).magnitude < positionThreshold)
+            {
+                transform.position = TargetPosition;
+                IsMoving = false;
+            }
+            else {
+                transform.position = transform.position + displacement;
+                
+            }
+        }
+	
+	}
+}
---
+++ Code/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+
+    private float minY = -3;
+    private float maxY = 0;
+
+    private float characterDepth = -1.0f;
+
+    [SerializeField]
+    private Camera MainCamera;
+
+    [SerializeField]
+    private Character P1; 
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 p1InitPos = new Vector3(-0.5f,-0.5f,characterDepth);
+        P1.transform.position = p1InitPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 tpos = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            float y = Mathf.Clamp(tpos.y, minY, maxY);
+            tpos.y = y;
+            tpos.z = characterDepth;
+
+            P1.SetTargetPosition(tpos);
+        }
+        
+	}
+}
